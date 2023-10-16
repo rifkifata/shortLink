2e5fb4bb790dd9ce3908a7fb8d245a7aab6637ce
Teuku Rifki Dhulul Fata <u073590@bca.co.id>
2023-10-16 14:20:25 +0700

    add CRUD

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const axios = require('axios')
 const {
     ObjectID
 } = require('mongodb')
+const e = require('express')
+const {
+    param
+} = require('express/lib/request')
+const {
+    get
+} = require('express/lib/response')
 const now = new Date();
 'use strict';
 require('dotenv').config()
@@ -21,131 +28,130 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.post('/', async (req, res) => {
+app.post('/short/', async (req, res) => {
+    console.log(Message("inProgress", "POST", ""))
+
+    const col = "short"
     const shortedPath = req.body.shortedPath
     const sourcePath = req.body.sourcePath
-
+    const author = req.body.author
 
     if (!shortedPath) {
-        res.json(errorMessage("shortedPath")).end
+        res.json(ErrorMessage("emptyShortedPath")).end()
     } else if (!sourcePath) {
-        res.json(errorMessage("sourcePath")).end
+        res.json(ErrorMessage("emptySourcePath")).end()
     }
 
-    axios.get(sourcePath)
-        .catch(function (error) {
-            if (error.response) {
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-                res.json(errorMessage("notFound")).end
-            }
-        });
+    //check duplicate
+    const duplicate = Get(col, shortedPath)
+    if (duplicate == false) {
+        const info = {
+            "Info: ": ErrorMessage("failedPost")
+        }
+        res.json(info).end()
+        console.log(info)
+    }
+
+    //check the url notfound
+    const checkUrl = CheckURL(sourcePath)
+    if (checkUrl) {
+        res.json(checkUrl).end()
+    }
 
     const body = {
-        "sourcePath": req.body.sourcePath,
-        "shortedPath": req.body.shortedPath,
+        "sourcePath": sourcePath,
+        "shortedPath": shortedPath,
+        "author": req.body.author ? author : null,
         "createdAt": now.toISOString(),
         "updatedAt": now.toISOString()
     }
 
-    const objectId = new ObjectID();
-    const col = req.params.col
-    let key = objectId.toString()
+    const post = Post(col, shortedPath, body)
 
-    console.log(`from collection: ${col} post key: ${key} with params ${JSON.stringify(req.params)}`)
-    const item = await db.collection(col).set(key, isi)
-    console.log(JSON.stringify(item, null, 2))
-    res.json(item).end()
-})
-
-// Delete an item
-app.delete('/:col/:key', async (req, res) => {
-    const col = req.params.col
-    const key = req.params.key
-    console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-    const item = await db.collection(col).delete(key)
-    console.log(JSON.stringify(item, null, 2))
-    res.json(item).end()
-})
-
-// Get a single item
-app.get('/getbykey/:col/:key', async (req, res) => {
-    const col = req.params.col
-    const key = req.params.key
-    console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
-    let item = await db.collection(col).get(key)
-    let props = item.props
-    delete props.updated
-    delete props.created
-    let newitem = {
-        key: key,
-        collection: col,
-        ...props
-    }
-    console.log(JSON.stringify(newitem, null, 2))
-    res.json(newitem).end()
-})
-
-//get All
-app.get('/getall/:col', async (req, res) => {
-    const col = req.params.col
-    console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
-    const items = await db.collection(col).list()
-    let result = items.results.map(a => a.key)
-    let currentArray = []
-
-    await Promise.all(
-        result.map(async (item) => {
-            currentArray.push(await db.collection(col).get(item))
-        })
-    )
-
-    currentArray.map(item => {
-        Object.assign(item, item.props)
-        delete item.props;
-        return item
-    })
-
-    let finalResult = {
-        "results": currentArray
-    }
-    res.json(finalResult).end()
-})
-
-// Update entire bike
-app.put("/:col/:key", async (req, res) => {
-    const key = req.params.key
-    const col = req.params.col
-    const now = new Date()
-
-    // get createdAt and updatedAt
-    let oldDate = await db.collection(col).get(key)
-    let createdAtOld = oldDate.props.createdAt
-    let updatedAtOld = oldDate.props.updatedAt
-
-    if (req.body.updatedAt) {
-        const mydate = req.body.updatedAt
-        updatedAtOld = new Date(mydate).toISOString()
+    if (post == true) {
+        const info = {
+            "Info: ": SuccessMessage("successPost", shortedPath)
+        }
+        res.json(post).end()
+        console.log(info)
     } else {
-        updatedAtOld = now.toISOString()
+        const info = {
+            "Info: ": ErrorMessage("failedPost")
+        }
+        res.sendStatus(404)
+        res.json(info).end()
+        console.log(`${info} /shorted/${JSON.stringify(shortedPath)} `)
     }
+})
 
-    // Delete existing object
-    await db.collection(col).delete(key)
-
-    //isi
-    const isi = {
-        ...req.body,
-        "updatedAt": updatedAtOld,
-        "createdAt": createdAtOld
+app.delete('/short/:shortedPath', async (req, res) => {
+    const key = req.params.shortedPath
+    console.log(Message("inProgress", "DELETE", key))
+    const del = Delete("shorted", key)
+    if (del == true) {
+        const info = {
+            "Info: ": SuccessMessage("successDelete", key)
+        }
+        res.json(info).end()
+        console.log(info)
+    } else {
+        const info = {
+            "Info: ": ErrorMessage("failedDelete")
+        }
+        res.sendStatus(404)
+        res.json(info).end()
+        console.log(`${info} /shorted/${JSON.stringify(key)} `)
     }
-    // Save new Object
-    const item = await db.collection(col).set(key, isi)
-    console.log(JSON.stringify(item, null, 2))
-    res.json(item).end()
-});
+})
 
+app.get('/short/:key', async (req, res) => {
+    const col = "short"
+    const key = req.params.shortedPath
+    console.log(Message("inProgress", "GET", req.params.shortedPath))
+
+    const get = Get(col, key)
+
+    if (get == true) {
+        const info = {
+            "Info: ": SuccessMessage("successGet", key)
+        }
+        res.json(get).end()
+        console.log(info)
+    } else {
+        const info = {
+            "Info: ": ErrorMessage("failedGet")
+        }
+        res.sendStatus(404)
+        res.json(info).end()
+        console.log(`${info} /shorted/${JSON.stringify(key)} `)
+    }
+})
+
+// //get All
+// app.get('/getall/:col', async (req, res) => {
+//     const col = req.params.col
+//     console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
+//     const items = await db.collection(col).list()
+//     let result = items.results.map(a => a.key)
+//     let currentArray = []
+
+//     await Promise.all(
+//         result.map(async (item) => {
+//             currentArray.push(await db.collection(col).get(item))
+//         })
+//     )
+
+//     currentArray.map(item => {
+//         Object.assign(item, item.props)
+//         delete item.props;
+//         return item
+//     })
+
+//     let finalResult = {
+//         "results": currentArray
+//     }
+//     res.json(finalResult).end()
+// })
 
 // Catch all handler for all other request.
 app.use('*', (req, res) => {
@@ -160,9 +166,66 @@ app.listen(port, () => {
     console.log(`index.js listening on ${port}`)
 })
 
-function errorMessage(err) {
-    if (err == "destPath") return "Destination Path Cannot be Empty"
-    if (err == "sourcePath") return "Source Path Cannot be Empty"
-    //if (err == "realPath") return "Source Path Cannot be Empty"
+function ErrorMessage(err) {
+    if (err == "emptyShortedPath") return "Shorted Path Cannot be Empty"
+    if (err == "emptySourcePath") return "Source Path Cannot be Empty"
+    if (err == "failedDelete") return "Failed to Delete, Data not Found"
+    if (err == "failedPost") return "Failed to Post, Duplicate Data"
+    if (err == "failedGet") return "Failed to Get, Data not Found"
+    if (err == "failedUrl") return "URL Error"
+}
 
+function Message(msg, progress, params) {
+    if (msg == "inProgress") return `try to ${progress} ${params}`
+}
+
+function SuccessMessage(msg, params) {
+    if (msg == "successDelete") return `Success Delete ${JSON.stringify(params)}`
+    if (msg == "successPost") return `Success Post ${JSON.stringify(params)}`
+    if (msg == "successGet") return `Success Get ${JSON.stringify(params)}`
+}
+
+function Delete(col, key) {
+    try {
+        return await db.collection(col).delete(key)
+    } catch (e) {
+        return e.message
+    }
+}
+
+function Post(col, key, body) {
+    try {
+        return await db.collection(col).set(key, body)
+    } catch (e) {
+        return e.message
+    }
+}
+
+function Get(col, key) {
+    try {
+        const item = await db.collection(col).get(key)
+        let props = item.props
+        delete props.updated
+        delete props.created
+        let newitem = {
+            key: key,
+            collection: col,
+            ...props
+        }
+        return newitem
+    } catch (e) {
+        return e.message
+    }
+}
+
+function CheckURL(path) {
+    axios.get(path)
+        .catch(function (error) {
+            if (error.response) {
+                console.log(error.response.data)
+                console.log(error.response.status)
+                console.log(error.response.headers)
+                return ErrorMessage("failedUrl")
+            }
+        })
 }
