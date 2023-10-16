93454322e62151a5169444a687dffc757bc889cf
Teuku Rifki Dhulul Fata <u073590@bca.co.id>
2023-10-16 15:02:44 +0700

    add async

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
+//
 app.post('/short/', async (req, res) => {
     console.log(Message("inProgress", "POST", ""))
 
