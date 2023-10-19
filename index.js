e9a5bee6abccf391ec8d21339c8ee6887ccb4017
Teuku Rifki Dhulul Fata <u073590@bca.co.id>
2023-10-19 18:11:32 +0700

    add get all

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ app.delete('/short/:shortedPath', async (req, res) => {
 app.get('/all/short', async (req, res) => {
     console.log(Message("inProgress", "GETALL", ""))
     const col = "short"
-    const items = GetAll(col)
+    const items = await GetAll(col)
     if (items == {}) {
         res.status(404)
         res.json(ErrorMessage("failedGet")).end()
